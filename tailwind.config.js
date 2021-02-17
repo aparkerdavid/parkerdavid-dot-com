991dccdd82fbe51347b17cef3d8e8bf1ba7d84f3
Parker Rueve <parkerrueve@gmail.com>
2021-02-17 00:08:26 -0500

    beton colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      beton: {
+        100: "#f5f2f0",
+        200: "#ded4cd",
+      },
+    },
     extend: {},
   },
   variants: {
