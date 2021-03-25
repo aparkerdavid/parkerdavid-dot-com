b6c63b505b9e40cad6619e1dfad419be639c6837
Parker Rueve <parkerrueve@gmail.com>
2021-03-25 18:19:14 -0400

    Remove dummy content

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber';
+import React from 'react';
 
 function BlockLink(props) {
   return (
     <div className="flex-grow">
       <Link href={props.href}>
-        <div className="w-36 h-36 border-gray-900 border cursor-pointer">
+        <div className="rounded-sm w-36 h-36 border-gray-900 border cursor-pointer">
           {props.children}
         </div>
       </Link>
     </div>
-  );
-}
-
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.001;
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={[150, 150, 150]}>
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"gray"} />
-    </mesh>
   );
 }
 
@@ -45,18 +24,14 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col justify-center items-center min-h-screen bg-beton-100">
-        <Canvas orthographic="true" className="h-96">
-          <pointLight position={[10, 10, 10]} intensity={10} />
-          <Box position={[0, 0, -500]} />
-        </Canvas>
         <h1>Parker David Rueve</h1>
         <main>
           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-            <BlockLink href="/beton">Beton Theme</BlockLink>
+            {/*<BlockLink href="/beton">Beton Theme</BlockLink>*/}
             <BlockLink href="https://stoic-franklin-71dc69.netlify.app">7 GUIs</BlockLink>
-            <BlockLink href="/structures">structures</BlockLink>
+            {/*<BlockLink href="/structures">structures</BlockLink>*/}
             <BlockLink href="/code">code</BlockLink>
-            <BlockLink href="/poasts">poasts</BlockLink>
+            {/*<BlockLink href="/poasts">poasts</BlockLink>*/}
             <BlockLink href="/cv">CV</BlockLink>
 
           </div>
