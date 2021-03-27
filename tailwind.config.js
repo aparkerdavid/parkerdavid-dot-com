d4df33ff263699998d8fab158ef9e272344aec64
Parker Rueve <parkerrueve@gmail.com>
2021-03-26 23:19:54 -0400

    Add beton colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,24 @@ module.exports = {
     colors: {
       'beige': '#f5f2f0',
       'beige-shade': "#ded4cd",
-      'white': "#ffffff"
+      'white': "#ffffff",
+      'black': "#000000",
+      'red': "#AA3731",
+      'red-shade': "#7F2925",
+      'green': "#448C27",
+      'green-shade': "#32681D",
+      'yellow': "#CB9000",
+      'yellow-shade': "#996D00",
+      'blue': "#325CC0",
+      'blue-shade': "#284A9A",
+      'magenta': "#9343B1",
+      'magenta-shade': "#75358D",
+      'cyan': "#0083B2",
+      'cyan-shade': "#006285",
+
     }
     },
     extend: {
-      colors: {
-        'beige': "#f5f2f0",
-        'beige-shade': "#ded4cd"
-    },
   },
   variants: {
     extend: {},
